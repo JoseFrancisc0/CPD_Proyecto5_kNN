355a9b3ecac7d04bb6de5592a3e9868f5963bfec
JoseFrancisc0 <jose.wong@utec.edu.pe>
2026-05-06 14:12:02 -0500

    dumpeo de metricas en csv

--- knn_sec.py
+++ knn_sec.py
@@ -2,7 +2,9 @@ from sklearn.datasets import load_digits
 from sklearn.model_selection import train_test_split
 from collections import Counter
 import numpy as np
+import pandas as pd
 import time
+import os
 
 def euclidean_distance(a, b):
     return np.sqrt(np.sum((a - b) ** 2))
@@ -42,3 +44,19 @@ gflops = flops / ((end_time - start_time) * 1e9)
 print(f"Accuracy: {accuracy:.4f}")
 print(f"Execution time (sequential): {t_total:.4f} sec")
 print(f"GFLOPs: {gflops:.4f} GFLOPs/s")
+
+results_df = pd.DataFrame([{
+    'Processes': 1,
+    'Subset_%': 100,
+    'N_train': len(X_train),
+    'N_test': len(X_test),
+    'T_comp (s)': t_total,
+    'T_comm (s)': 0.0,
+    'T_total (s)': t_total,
+    'GFLOP/s': gflops,
+    'Accuracy': accuracy
+}])
+
+filename = "knn_results.csv"
+write_header = not os.path.exists(filename)
+results_df.to_csv(filename, mode='a', header=write_header, index=False)
